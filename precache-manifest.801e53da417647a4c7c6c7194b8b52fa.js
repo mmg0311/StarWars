31eda80cbf32e803fd4f8f42addf0e52727e7116
mmg0311 <mohakgadge@gmail.com>
2020-01-20 19:41:14 +0530

    Updates

---
+++ precache-manifest.801e53da417647a4c7c6c7194b8b52fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c69ba08f68e249cd5024b918b3046749",
+    "url": "/StarWars/index.html"
+  },
+  {
+    "revision": "87bf1ec36224ef82e6a6",
+    "url": "/StarWars/static/js/2.7f0df2b5.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/StarWars/static/js/2.7f0df2b5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "55edf44c39c7f26ab4ec",
+    "url": "/StarWars/static/js/main.c4f7fa37.chunk.js"
+  },
+  {
+    "revision": "4152f5c8c223ceb4c335",
+    "url": "/StarWars/static/js/runtime-main.a7e160fe.js"
+  }
+]);
